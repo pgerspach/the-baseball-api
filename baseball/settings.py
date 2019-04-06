40a61d3489fc9c6f8d13ebc92f9e2dd0ddd0566b
pgerspach <pgerspac@alumni.nd.edu>
2019-04-05 22:35:43 -0400

    removed all of the stupid database settings

--- baseball/settings.py
+++ baseball/settings.py
@@ -75,11 +75,6 @@ WSGI_APPLICATION = 'baseball.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],
     }
 }
 

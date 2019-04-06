f61502427e7493a9febeb7a189b148635c73b424
pgerspach <pgerspac@alumni.nd.edu>
2019-04-05 22:31:07 -0400

    FIXED MISSING COMMA

--- baseball/settings.py
+++ baseball/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DB_NAME'],
         'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD']
+        'PASSWORD': os.environ['DB_PASSWORD'],
         'HOST': os.environ['DB_HOST'],
         'PORT': os.environ['DB_PORT'],
     }

94a7e5d70cce775151645646e8b1df8e4db73566
pgerspach <pgerspac@alumni.nd.edu>
2019-04-07 08:19:21 -0400

    change debug

--- baseball/settings.py
+++ baseball/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = os.environ['DJANGO_DEBUG']
 
 ALLOWED_HOSTS = ["localhost", "172.0.0.1", "thebaseballapi.herokuapp.com"]
 
